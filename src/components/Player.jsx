972574534646e09aa6ebf7e53972bcdf4a446c8c
benedictleejr <belasolee@gmail.com>
2025-06-10 15:21:27 +0800

    Add refs and timers to app

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,19 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+  const [player, setPlayer] = useState("unknown entity");
+
+  const handleSetName = () => {
+    setPlayer(playerName.current.value);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {player || "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleSetName}>Set Name</button>
       </p>
     </section>
   );
